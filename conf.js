8263233d1e5b638a2c260b6f6d2d984a866baf33
GustavoDuranBR <gustavoduran22@icloud.com>
2022-09-25 22:29:30 -0300

    Correção do conf.js

--- conf.js
+++ conf.js
@@ -1,18 +1,5 @@
 var HtmlReporter = require('protractor-beautiful-reporter');
 
-<<<<<<< HEAD
-exports.config = {
-   seleniumAddress: 'http://localhost:4444/wd/hub',
-   specs: ['specs/loginUsers.spec.js'],
-
-   onPrepare: function () {
-      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
-      jasmine.getEnv().addReporter(new HtmlReporter({
-         baseDirectory: 'tmp/screenshots',
-         takeScreenShotsOnlyForFailedSpecs: true
-      }).getJasmine2Reporter());
-   }
-=======
 exports.config ={
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs:['specs/loginUsers.spec.js'],
@@ -23,5 +10,4 @@ exports.config ={
             baseDirectory: 'tmp/screenshots'
         }));
      }
->>>>>>> c61198e2b899f002143007ff783f5271b21ec601
 };
