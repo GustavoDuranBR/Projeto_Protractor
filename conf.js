25f5dff01089661d300a23ba0be5babfaa318647
GustavoDuranBR <gustavoduran22@icloud.com>
2022-09-26 23:40:28 -0300

    Melhoria do protractor-beautiful-reporter

--- conf.js
+++ conf.js
@@ -1,13 +1,22 @@
-var HtmlReporter = require('protractor-beautiful-reporter');
-
-exports.config ={
+exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs:['specs/loginUsers.spec.js'],
-    
-    onPrepare: function(){
-        // Add a screenshot reporter and store screenshots to `/tmp/`:
+    specs: ['specs/loginUsers.spec.js'],
+    framework: 'jasmine',
+
+    onPrepare: function () {
+        let HtmlReporter = require('protractor-beautiful-reporter');
         jasmine.getEnv().addReporter(new HtmlReporter({
-            baseDirectory: 'tmp/screenshots'
-        }));
-     }
+            baseDirectory: 'reports_new',
+            screenshotsSubfolder: 'screenshotsOnFailure',
+            takeScreenShotsOnlyForFailedSpecs: false,
+            jsonsSubfolder: 'jsonFiles',
+            excludeSkippedSpecs: false,
+            preserveDirectory: false,
+            clientDefaults: {
+                showTotalDurationIn: "header",
+                totalDurationFormat: "h:m:s",
+                gatherBrowserLogs: true
+            },
+        }).getJasmine2Reporter());
+    }
 };
